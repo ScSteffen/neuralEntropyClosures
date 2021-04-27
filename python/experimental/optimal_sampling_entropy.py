a6124e6407e2d9518caaa6ab58073463743dab50
Steffen Schotthöfer <steffen.schottthoefer@kit.edu>
2021-04-27 09:47:54 +0200

    added better kernel initializer, added smart entropy sampling

--- python/experimental/optimal_sampling_entropy.py
+++ python/experimental/optimal_sampling_entropy.py
@@ -26,9 +26,45 @@ def exp_func(x):
     return np.exp(x)  # / np.exp(3)
 
 
-def sample_data(min_x, max_x, tol, function, grad):
+def get_errors(s_x, function, grad, tol):
+    eL = []
+    tL = []
+    for i in range(len(s_x) - 1):
+        t1 = 1 / (grad(s_x[i + 1]) - grad(s_x[i])) * (function(s_x[i]) - s_x[i] * grad(s_x[i]) - (
+                function(s_x[i + 1]) - s_x[i + 1] * grad(s_x[i + 1])))
+        e1 = function(t1) - (function(s_x[i]) + (t1 - s_x[i]) * grad(s_x[i]))
+
+        # error in case that the approximation overshoots
+        tmp1 = function(s_x[i + 1])
+        tmp2 = function(s_x[i])
+        grad_intermediate = (tmp1 - tmp2) / (s_x[i + 1] - s_x[i])
+
+        def F(x):
+            return 0.5 * (grad_intermediate - grad(x)) * (grad_intermediate - grad(x))
+
+        result = scipy.optimize.minimize_scalar(method="bounded", fun=F, bounds=[s_x[i], s_x[i + 1]])
+        if not result.success:
+            ValueError("Optimization unsuccessful!")
+            print(grad_intermediate)
+            print((s_x[i + 1] - s_x[i]) / 2)
+            print("----")
+
+        t2 = float(result.x)
+        e2 = function(s_x[i]) + grad_intermediate * (t2 - s_x[i]) - function(t2)
+
+        if e1 > e2:
+            eL.append(e1)
+            tL.append(t1)
+        else:
+            eL.append(e2)
+            tL.append(t2)
+
+    return [eL, tL]
+
+
+def sample_data_entropy_M1(u_min, u_max, tol):
     """
-    performs the sampling algorithm
+    performs the smart sampling algorithm on the entropy closure problem
     """
 
     s_x = [min_x, max_x]
@@ -86,45 +122,8 @@ def sample_data(min_x, max_x, tol, function, grad):
     return s_x
 
 
-def get_errors(s_x, function, grad, tol):
-    eL = []
-    tL = []
-    for i in range(len(s_x) - 1):
-        t1 = 1 / (grad(s_x[i + 1]) - grad(s_x[i])) * (function(s_x[i]) - s_x[i] * grad(s_x[i]) - (
-                function(s_x[i + 1]) - s_x[i + 1] * grad(s_x[i + 1])))
-        e1 = function(t1) - (function(s_x[i]) + (t1 - s_x[i]) * grad(s_x[i]))
-
-        # error in case that the approximation overshoots
-        tmp1 = function(s_x[i + 1])
-        tmp2 = function(s_x[i])
-        grad_intermediate = (tmp1 - tmp2) / (s_x[i + 1] - s_x[i])
-
-        def F(x):
-            return 0.5 * (grad_intermediate - grad(x)) * (grad_intermediate - grad(x))
-
-        result = scipy.optimize.minimize_scalar(method="bounded", fun=F, bounds=[s_x[i], s_x[i + 1]])
-        if not result.success:
-            ValueError("Optimization unsuccessful!")
-            print(grad_intermediate)
-            print((s_x[i + 1] - s_x[i]) / 2)
-            print("----")
-
-        t2 = float(result.x)
-        e2 = function(s_x[i]) + grad_intermediate * (t2 - s_x[i]) - function(t2)
-
-        if e1 > e2:
-            eL.append(e1)
-            tL.append(t1)
-        else:
-            eL.append(e2)
-            tL.append(t2)
-
-    return [eL, tL]
-
-
 def main():
     # x = sample_data(-10, 10, 0.0001, quad_func, quad_func_grad)
-    tolerance = 0.1
 
     # test the entropyTools
     batchSize = 200
@@ -139,7 +138,12 @@ def main():
 
     utils.plot1D(u[:, 1], [h], ['h'], 'sanity_check', log=False, folder_name="figures")
     # Looking good
-    
+    u_lower = -0.999
+    u_upper = 0.999
+    tolerance = 0.1
+
+    [u_train, alpha_train, h_train] = sample_data_entropy_M1(u_lower, u_upper, tolerance)
+
     return 0
 
 
