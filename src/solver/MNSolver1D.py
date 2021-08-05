1d3d33a3bfca388fab4240a851e70adecc9da2bd
ScSteffen <steffen.schotthoefer@outlook.de>
2021-08-05 11:49:44 +0200

    tidy up of other modules

--- src/solver/MNSolver1D.py
+++ src/solver/MNSolver1D.py
@@ -19,7 +19,7 @@ from joblib import Parallel, delayed
 # inpackage imports
 # from neuralClosures.configModel import initNeuralClosure
 from src import math
-from src.neuralClosures.configModel import init_neural_closure
+from src.neuralClosures.configmodel import init_neural_closure
 
 num_cores = multiprocessing.cpu_count()
 
