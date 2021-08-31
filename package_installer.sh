97cb5657724a6901dd0b3a294431d7b20fd98f41
ScSteffen <steffen.schotthoefer@outlook.de>
2021-08-31 11:47:51 +0200

    added script to automatically install all neccessary python packages for callNeuralClosure.py

---
+++ package_installer.sh
@@ -0,0 +1,6 @@
+# bash script to install all neccessary python packages
+
+# first all conda packages
+
+conda install numpy pandas matplotlib
+pip install tensorflow-gpu==2.6 scikit-learn gitpython
