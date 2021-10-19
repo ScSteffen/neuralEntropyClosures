updated load model routine. fixed bug in output scaling

src/networks/basenetwork.py
@@ -44,7 +44,9 @@ class BaseNetwork:
     input_decorrelation: bool  # flag to turn on decorrelation of input variables
 
     def __init__(self, normalized: bool, polynomial_degree: int, spatial_dimension: int,
-                 width: int, depth: int, loss_combination: int, save_folder: str, input_decorrelation: bool):
+                 width: int, depth: int, loss_combination: int, save_folder: str, input_decorrelation: bool,
+                 scale_active: bool):
+        self.scale_active = scale_active
         self.normalized = normalized
         self.input_decorrelation = input_decorrelation
         if self.input_decorrelation:
@@ -140,9 +142,8 @@ class BaseNetwork:
             tf.keras.backend.set_floatx('float32')
 
         # Create callbacks
-        mc_best = tf.keras.callbacks.ModelCheckpoint(self.folder_name + '/best_model', monitor='loss', mode='min',
-                                                     save_best_only=True,
-                                                     verbose=verbosity)  # , save_weights_only = True, save_freq = 50, verbose=0)
+        mc_best = tf.keras.callbacks.ModelCheckpoint(self.folder_name + '/best_model', monitor='val_loss', mode='min',
+                                                     save_best_only=True, verbose=verbosity)
         es = tf.keras.callbacks.EarlyStopping(monitor='loss', mode='min', min_delta=0.0001, patience=10,
                                               verbose=1)
 
@@ -162,10 +163,10 @@ class BaseNetwork:
                 # assemble callbacks
                 callbackList = []
                 csv_logger = self.create_csv_logger_cb()
-                if verbosity == 1:
-                    callbackList = [mc_best, csv_logger]
-                else:
+                if verbosity == 0:
                     callbackList = [mc_best, LossAndErrorPrintingCallback(), csv_logger]
+                else:
+                    callbackList = [mc_best, csv_logger]
 
                 # start Training
                 self.history = self.call_training(val_split=val_split, epoch_size=epoch_count, batch_size=batch_size,
@@ -286,12 +287,14 @@ class BaseNetwork:
         if filename != None:
             usedFileName = filename
 
-        usedFileName = usedFileName + '/best_model'
+        usedFileName = usedFileName + '/best_model/'
 
         if path.exists(usedFileName) == False:
             print("Model does not exists at this path: " + usedFileName)
             exit(1)
-        self.model.load_weights(usedFileName)
+        model = tf.keras.models.load_model(usedFileName, custom_objects={"CustomModel": self.model})
+        # self.model.load_weights(usedFileName)
+        self.model = model
         print("Model loaded from file ")
         return 0
 
@@ -425,8 +428,9 @@ class BaseNetwork:
         return: True, if run successfully. Prints several plots and pictures to file.
         """
 
-        # [u_pred, alpha_pred, h_pred] = self.call_network(u_test)
-        alpha = self.call_network(u_test)
+        [u_pred, alpha_pred, h_pred] = self.call_network(u_test)
+
+        # alpha = self.call_network(u_test)
 
         # create the loss functions
         def pointwise_diff(true_samples, pred_samples):

src/networks/mk14.py
@@ -27,7 +27,7 @@ class MK14Network(BaseNetwork):
     '''
 
     def __init__(self, normalized: bool, input_decorrelation: bool, polynomial_degree: int, spatial_dimension: int,
-                 width: int, depth: int, loss_combination: int, save_folder: str = ""):
+                 width: int, depth: int, loss_combination: int, save_folder: str = "", scale_active: bool = True):
         if save_folder == "":
             custom_folder_name = "MK14_N" + str(polynomial_degree) + "_D" + str(spatial_dimension)
         else:
@@ -35,7 +35,7 @@ class MK14Network(BaseNetwork):
         super(MK14Network, self).__init__(normalized=normalized, polynomial_degree=polynomial_degree,
                                           spatial_dimension=spatial_dimension, width=width, depth=depth,
                                           loss_combination=loss_combination, save_folder=custom_folder_name,
-                                          input_decorrelation=input_decorrelation)
+                                          input_decorrelation=input_decorrelation, scale_active=scale_active)
 
     def create_model(self) -> bool:
         input_initializer = tf.keras.initializers.LecunNormal()

src/networks/mk15.py
@@ -14,6 +14,7 @@ from tensorflow import keras as keras
 from tensorflow.keras import layers
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from os import path
 
 from src.networks.basenetwork import BaseNetwork
 from src.networks.customlosses import MonotonicFunctionLoss, RelativeMAELoss
@@ -24,7 +25,7 @@ from src.networks.customlayers import MeanShiftLayer, DecorrelationLayer
 class MK15Network(BaseNetwork):
 
     def __init__(self, normalized: bool, input_decorrelation: bool, polynomial_degree: int, spatial_dimension: int,
-                 width: int, depth: int, loss_combination: int, save_folder: str = ""):
+                 width: int, depth: int, loss_combination: int, save_folder: str = "", scale_active: bool = True):
         if save_folder == "":
             custom_folder_name = "MK15_N" + str(polynomial_degree) + "_D" + str(spatial_dimension)
         else:
@@ -32,7 +33,7 @@ class MK15Network(BaseNetwork):
         super(MK15Network, self).__init__(normalized=normalized, polynomial_degree=polynomial_degree,
                                           spatial_dimension=spatial_dimension, width=width, depth=depth,
                                           loss_combination=loss_combination, save_folder=custom_folder_name,
-                                          input_decorrelation=input_decorrelation)
+                                          input_decorrelation=input_decorrelation, scale_active=scale_active)
 
     def create_model(self) -> bool:
 
@@ -179,8 +180,25 @@ class MK15Network(BaseNetwork):
                  h_predicted, dim = (nS x 1)
         """
         u_reduced = u_complete[:, 1:]  # chop of u_0
-        alpha_predicted = self.model(u_reduced)
-        alpha_complete_predicted = self.model.reconstruct_alpha(alpha_predicted)
+        [alpha_predicted, mono_loss, u_predicted, h_prediced] = self.model(u_reduced)
+        alpha_complete_predicted = self.model.reconstruct_alpha(tf.cast(alpha_predicted, dtype=tf.float64))
         u_complete_reconstructed = self.model.reconstruct_u(alpha_complete_predicted)
 
-        return [u_complete_reconstructed, alpha_complete_predicted]
+        return [u_complete_reconstructed, alpha_complete_predicted, h_prediced]
+
+    def load_model(self, filename=None):
+        usedFileName = self.folder_name
+        if filename != None:
+            usedFileName = filename
+
+        usedFileName = usedFileName + '/best_model/'
+
+        if path.exists(usedFileName) == False:
+            print("Model does not exists at this path: " + usedFileName)
+            exit(1)
+        model = tf.keras.models.load_model(usedFileName, custom_objects={"CustomModel": self.model,
+                                                                         "MonotonicFunctionLoss": MonotonicFunctionLoss})
+        # self.model.load_weights(usedFileName)
+        self.model = model
+        print("Model loaded from file ")
+        return 0