7b22d57e7fea716c78806502b9effbe24b1f5427
ScSteffen <steffen.schotthoefer@outlook.de>
2021-10-20 10:01:27 +0200

    fixed scaler initilizer

--- src/networks/basenetwork.py
+++ src/networks/basenetwork.py
@@ -141,7 +141,7 @@ class BaseNetwork:
         with open(scaling_file_name, 'w') as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
             writer.writerow([self.scaler_min, self.scaler_max])
-       
+
         # Set double precision training for CPU training #TODO
         if processing_mode == 0:
             tf.keras.backend.set_floatx('float32')
@@ -306,7 +306,7 @@ class BaseNetwork:
                 scaling_data = row
         self.scaler_min = float(scaling_data[0])
         self.scaler_max = float(scaling_data[1])
-
+        self.create_model()
         used_file_name = used_file_name + '/best_model/'
 
         if not path.exists(used_file_name):
--- src/networks/mk15.py
+++ src/networks/mk15.py
@@ -203,7 +203,7 @@ class MK15Network(BaseNetwork):
                 scaling_data = row
         self.scaler_min = float(scaling_data[0])
         self.scaler_max = float(scaling_data[1])
-
+        self.create_model()
         used_file_name = used_file_name + '/best_model/'
 
         if path.exists(used_file_name) == False:
