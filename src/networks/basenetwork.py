fixed bug when loading training data with preset scaling. fixed bug in internal scaling for MK11 networks

src/networks/basenetwork.py
@@ -158,7 +158,7 @@ class BaseNetwork:
             tf.keras.backend.set_floatx('float32')
 
         # Create callbacks
-        mc_best = tf.keras.callbacks.ModelCheckpoint(self.folder_name + '/best_model', monitor='val_loss', mode='min',
+        mc_best = tf.keras.callbacks.ModelCheckpoint(self.folder_name + '/best_model', monitor='loss', mode='min',
                                                      save_best_only=True, verbose=verbosity)
         es = tf.keras.callbacks.EarlyStopping(monitor='loss', mode='min', min_delta=0.0001, patience=10,
                                               verbose=1)
@@ -338,18 +338,15 @@ class BaseNetwork:
         return True
 
     def load_training_data(self, shuffle_mode: bool = False, sampling: int = 0, load_all: bool = False,
-                           normalized_data: bool = False, scaled_output: bool = False,
+                           normalized_data: bool = False,
                            train_mode: bool = False) -> bool:
         """
         Loads the training data
         params: normalized_moments = load normalized data  (u_0=1)
                 shuffle_mode = shuffle loaded Data  (yes,no)
                 alpha_sampling = use data uniformly sampled in the space of Lagrange multipliers.
-                scaled_output: bool = Determines if entropy is scaled to range (0,1) (other outputs scaled accordingly)
-
         return: True, if loading successful
         """
-        self.scale_active = scaled_output
         self.training_data = []
 
         ### Create trainingdata filename"
@@ -418,29 +415,36 @@ class BaseNetwork:
             print("Shifting the data accordingly if network architecture is MK15 or newer...")
         else:
             print("Warning: Mean of training data moments was not computed")
-        if train_mode:
-            self.training_data_preprocessing(scaled_output=scaled_output)
-
         return True
 
-    def training_data_preprocessing(self, scaled_output: bool = False) -> bool:
+    def training_data_preprocessing(self, scaled_output: bool = False, model_loaded: bool = False) -> bool:
         """
         Performs a scaling on the output data (h) and scales alpha correspondingly. Sets a scale factor for the
         reconstruction of u during training and execution
+        params:            scaled_output: bool = Determines if entropy is scaled to range (0,1) (other outputs scaled accordingly)
+                           model_loaded: bool = Determines if models is loaded from file. Then scaling data from file is used
+        returns: True if terminated successfully
         """
-        self.scaler_max = 1.0
-        self.scaler_min = 0.0
-        self.training_data.append(self.training_data[1])
+        self.scale_active = scaled_output
         if scaled_output:
-            scaler = MinMaxScaler()
-            scaler.fit(self.training_data[2])
-            self.scaler_max = float(scaler.data_max_)
-            self.scaler_min = float(scaler.data_min_)
-            self.training_data[2] = scaler.transform(self.training_data[2])
-            self.training_data[1] = (self.training_data[1] - self.scaler_min) / (self.scaler_max - self.scaler_min)
-            # * 2 - 1
-        print("Output of network has internal scaling with h_max=" + str(self.scaler_max) + " and h_min=" + str(
-            self.scaler_min))
+            if not model_loaded:
+                scaler = MinMaxScaler()
+                scaler.fit(self.training_data[2])
+                self.scaler_max = float(scaler.data_max_)
+                self.scaler_min = float(scaler.data_min_)
+            # scale to [0,1]
+            self.training_data[2] = (self.training_data[2] - self.scaler_min) / (self.scaler_max - self.scaler_min)
+            # scale correspondingly
+            self.training_data[1] = self.training_data[1] / (self.scaler_max - self.scaler_min)
+            print("Output of network has internal scaling with h_max=" + str(self.scaler_max) + " and h_min=" + str(
+                self.scaler_min))
+            print("New h_min= " + str(self.training_data[2].min()) + ". New h_max= " + str(
+                self.training_data[2].max()))
+            print("New alpha_min= " + str(self.training_data[1].min()) + ". New alpha_max= " + str(
+                self.training_data[1].max()))
+        else:
+            self.scaler_max = 1.0
+            self.scaler_min = 0.0
         return True
 
     def get_training_data(self):
@@ -572,7 +576,7 @@ class BaseNetwork:
 
     """
     def normalize_data(self):
-
+    
         # load data
         #
         [u_t, alpha_t, h_t] = self.training_data
@@ -583,17 +587,17 @@ class BaseNetwork:
         u_non_normal = tf.constant(u_t, dtype=tf.float64)
         alpha_non_normal = tf.constant(alpha_t, dtype=tf.float64)
         h_non_normal = tf.constant(h_t, dtype=tf.float64)
-
+    
         # scale u and alpha
         u_normal = self.model.scale_u(u_non_normal, tf.math.reciprocal(u_non_normal[:, 0]))  # downscaling
         alpha_normal = self.model.scale_alpha(alpha_non_normal, tf.math.reciprocal(u_non_normal[:, 0]))
-
+    
         # compute h
         f_quad = tf.math.exp(tf.tensordot(alpha_normal, mBasis, axes=([1], [0])))  # alpha*m
         tmp = tf.tensordot(f_quad, qWeights, axes=([1], [1]))  # f*w
         tmp2 = tf.math.reduce_sum(tf.math.multiply(alpha_normal, u_normal), axis=1, keepdims=True)
         h_normal = tmp2 - tmp
-
+    
         self.training_data = [u_normal[:, 1:], alpha_normal[:, 1:], h_normal]
         return 0
     """

src/networks/custommodels.py
@@ -31,8 +31,7 @@ class EntropyModel(tf.keras.Model, ABC):
 
     def __init__(self, core_model: tf.keras.Model, polynomial_degree: int = 1, spatial_dimension: float = 1.0,
                  reconstruct_u: bool = False, scaler_min: float = 0.0, scaler_max: float = 1.0,
-                 scale_active: bool = True,
-                 **opts):
+                 scale_active: bool = True, subclass=False, **opts):
         super(EntropyModel, self).__init__()
         # Member is only the model we want to wrap with sobolev execution
         self.core_model = core_model  # must be a compiled tensorflow model
@@ -44,7 +43,8 @@ class EntropyModel(tf.keras.Model, ABC):
         self.scale_active = scale_active
         self.derivative_scale_factor = tf.constant((scaler_max - scaler_min) * 0.5, dtype=tf.float64)
 
-        print("Model output alpha will be scaled by factor " + str(self.derivative_scale_factor.numpy()))
+        if not subclass:
+            print("Model output alpha will be scaled by factor " + str(self.derivative_scale_factor.numpy()))
         if spatial_dimension == 1:
             [quad_pts, quad_weights] = math.qGaussLegendre1D(10 * polynomial_degree)  # dims = nq
             m_basis = math.computeMonomialBasis1D(quad_pts, self.poly_degree)  # dims = (N x nq)
@@ -242,8 +242,10 @@ class SobolevModel(EntropyModel):
                  scale_active: bool = True, **opts):
         super(SobolevModel, self).__init__(core_model=core_model, polynomial_degree=polynomial_degree,
                                            spatial_dimension=spatial_dimension, reconstruct_u=reconstruct_u,
-                                           scaler_min=scaler_min, scaler_max=scaler_max, scale_active=scale_active)
+                                           scaler_min=scaler_min, scaler_max=scaler_max, scale_active=scale_active,
+                                           subclass=True)
         self.derivative_scale_factor = tf.constant(scaler_max - scaler_min, dtype=tf.float64)
+        print("Model output alpha and h will be scaled by factor " + str(self.derivative_scale_factor.numpy()))
 
     def call(self, x: Tensor, training=False) -> list:
         """
@@ -260,16 +262,16 @@ class SobolevModel(EntropyModel):
 
         if self.enable_recons_u:
             if self.scale_active:
-                print("(Scaled) reconstruction of U enabled")
+                print("Scaled reconstruction of u enabled")
                 alpha64 = tf.math.scalar_mul(self.derivative_scale_factor, tf.cast(alpha, dtype=tf.float64, name=None))
             else:
-                print("Reconstruction of U enabled")
+                print("Reconstruction of u enabled")
                 alpha64 = tf.cast(alpha, dtype=tf.float64, name=None)
             alpha_complete = self.reconstruct_alpha(alpha64)
             u_complete = self.reconstruct_u(alpha_complete)
             res = u_complete[:, 1:]  # cutoff the 0th order moment, since it is 1 by construction
         else:
-            print("Reconstruction of U disabled. Output 3 is meaningless")
+            print("Reconstruction of u disabled. Output 3 is meaningless")
             res = alpha
         return [h, alpha, res]
 

src/networks/mk11.py
@@ -107,7 +107,7 @@ class MK11Network(BaseNetwork):
                                                   )(net_input_x)
             # Wz+Wx+b
             out: Tensor = layers.Add()([weighted_sum_x, weighted_nn_sum_z])
-            if self.scaler_max - self.scaler_min != 1.0:  # if output is scaled, use relu.
+            if self.scale_active:  # if output is scaled, use relu.
                 out = tf.keras.activations.relu(out)
             return out
 
@@ -181,12 +181,17 @@ class MK11Network(BaseNetwork):
         Calls training depending on the MK model
         '''
         x_data = self.training_data[0]
+        [h, alpha, u] = self.model(x_data[:1000])
+
         # y_data = [h,alpha,u, alpha (for KLDivergence)]
         y_data = [self.training_data[2], self.training_data[1], self.training_data[0]]  # , self.trainingData[1]]
         self.history = self.model.fit(x=x_data, y=y_data,
                                       validation_split=val_split, epochs=epoch_size,
                                       batch_size=batch_size, verbose=verbosity_mode,
                                       callbacks=callback_list, shuffle=True)
+
+        [h, alpha, u] = self.model(x_data)
+
         return self.history
 
     def select_training_data(self):

src/networks/mk15.py
@@ -120,7 +120,7 @@ class MK15Network(BaseNetwork):
         '''
         x_data = self.training_data[0]
         # t1 = self.training_data
-        t2 = self.model(self.training_data[1][:1000, :])
+        # t2 = self.model(self.training_data[1][:1000, :])
         # print(t2[2] - tf.constant(self.training_data[0][:1000, :], dtype=tf.float64))  # sanity check u
         # print(t2[1] - tf.constant(self.training_data[1][:1000, :], dtype=tf.float32))  # sanity check alpha
         # print(t2[3] - tf.constant(self.training_data[2][:1000, :], dtype=tf.float64))  # sanity check h
@@ -142,30 +142,36 @@ class MK15Network(BaseNetwork):
     def select_training_data(self):
         return [True, True, True]
 
-    def training_data_preprocessing(self, scaled_output: bool = False) -> bool:
+    def training_data_preprocessing(self, scaled_output: bool = False, model_loaded: bool = False) -> bool:
         """
-        Performs a scaling on the output data alpha. Sets a scale factor for the
+        Performs a scaling on the output data (h) and scales alpha correspondingly. Sets a scale factor for the
         reconstruction of u during training and execution
+        params:            scaled_output: bool = Determines if entropy is scaled to range (0,1) (other outputs scaled accordingly)
+                           model_loaded: bool = Determines if models is loaded from file. Then scaling data from file is used
+        returns: True if terminated successfully
         """
-        self.scaler_max = 1.0
-        self.scaler_min = 0.0
-        # self.training_data.append(self.training_data[1])
+        self.scale_active = scaled_output
         if scaled_output:
-            new_len = self.training_data[1].shape[0] * self.training_data[1].shape[1]
-            output = np.reshape(self.training_data[1], (new_len, 1))
-            scaler = MinMaxScaler()
-            scaler.fit(output)
-            self.scaler_max = float(scaler.data_max_)
-            self.scaler_min = float(scaler.data_min_)
+            if not model_loaded:
+                # sup norm scaling
+                new_len = self.training_data[1].shape[0] * self.training_data[1].shape[1]
+                output = np.reshape(self.training_data[1], (new_len, 1))
+                scaler = MinMaxScaler()
+                scaler.fit(output)
+                self.scaler_max = float(scaler.data_max_)
+                self.scaler_min = float(scaler.data_min_)
             # scale to [-1,1]
             self.training_data[1] = -1.0 + 2 / (self.scaler_max - self.scaler_min) * (
                     self.training_data[1] - self.scaler_min)
-            # 2 * self.training_data[1] / (self.scaler_max - self.scaler_min)
-        print(
-            "Output of network has internal scaling with alpha_max= " + str(self.scaler_max) + " and alpha_min= " + str(
-                self.scaler_min))
-        print("New alpha_min= " + str(self.training_data[1].min()) + ". New alpha_max= " + str(
-            self.training_data[1].max()))
+            print(
+                "Output of network has internal scaling with alpha_max= " + str(
+                    self.scaler_max) + " and alpha_min= " + str(
+                    self.scaler_min))
+            print("New alpha_min= " + str(self.training_data[1].min()) + ". New alpha_max= " + str(
+                self.training_data[1].max()))
+        else:
+            self.scaler_max = 1.0
+            self.scaler_min = 0.0
         return True
 
     def call_network(self, u_complete):