505aa40d0c9e6210e87d78b0bfb22eee5116b754
ScSteffen <steffen.schotthoefer@outlook.de>
2021-11-01 13:39:50 +0100

    added 1D models that are used in paper

---
+++ models/_simulation/mk15_M2_1D_normal/runScript.sh
@@ -0,0 +1,19 @@
+python callNeuralClosure.py \
+--sampling=0 \
+--batch=500 \
+--curriculum=1 \
+--degree=2 \
+--epoch=100000 \
+--folder=tmp \
+--loadModel=1 \
+--model=15 \
+--normalized=1 \
+--scaledOutput=1 \
+--decorrInput=1 \
+--objective=2 \
+--processingmode=0 \
+--spatialDimension=1 \
+--training=1 \
+--verbosity=2 \
+--networkwidth=50 \
+--networkdepth=2
