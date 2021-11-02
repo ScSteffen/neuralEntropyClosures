12f41f33d57dbee00f9bef8b83b20854bc53714b
ScSteffen <steffen.schotthoefer@outlook.de>
2021-11-02 12:01:44 +0100

    added 2D models that are used in paper

---
+++ models/_simulation/mk15_M1_2D_normal_gaussian/runScript_001_.sh
@@ -0,0 +1,19 @@
+python callNeuralClosure.py \
+--sampling=0 \
+--batch=200 \
+--curriculum=1 \
+--degree=1 \
+--epoch=10 \
+--folder=0_mk15_m1_2D_paper_sm \
+--loadModel=1 \
+--model=15 \
+--normalized=1 \
+--scaledOutput=1 \
+--decorrInput=0 \
+--objective=2 \
+--processingmode=1 \
+--spatialDimension=2 \
+--training=1 \
+--verbosity=1 \
+--networkwidth=100 \
+--networkdepth=3
