c4d3a6dc37c4e7a93f333e007d35ce997881425d
ScSteffen <steffen.schotthoefer@outlook.de>
2021-12-13 13:16:58 +0100

    changed simulation end time for error computation banach

--- callHybridSolver.py
+++ callHybridSolver.py
@@ -45,7 +45,7 @@ def main():
 
     if options.spatial_dimension == 1:
         solver = MNSolver1D.MNSolver1D(traditional=False, polyDegree=options.degree, model_mk=options.model)
-        solver.solve(maxIter=20000, t_end=10)
+        solver.solve(max_iter=20000, t_end=0.1)
     if options.spatial_dimension == 2:
         solver = MNSolver2D.MNSolver2D(traditional=False, model_mk=options.model)
         solver.solve(maxIter=2000, t_end=1)
