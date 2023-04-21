9581f13e194a228cf59df6ad23017ba8dc8f9bf5
Steffen <steffen.schotthoefer@kit.edu>
2023-04-21 10:56:51 +0200

    added negative input

--- src/networks/mk11.py
+++ src/networks/mk11.py
@@ -94,21 +94,17 @@ class MK11Network(BaseNetwork):
         if self.input_decorrelation:  # input data decorellation and shift
             hidden = MeanShiftLayer(input_dim=self.input_dim, mean_shift=self.mean_u, name="mean_shift")(input_)
             x = DecorrelationLayer(input_dim=self.input_dim, ev_cov_mat=self.cov_ev, name="decorrelation")(hidden)
-            # First Layer is a std dense layer
-            hidden = layers.Dense(self.model_width, activation="elu", kernel_initializer=input_initializer,
-                                  kernel_regularizer=None, use_bias=True,
-                                  bias_initializer=input_initializer,
-                                  bias_regularizer=None, name="layer_-1_input")(x)
-        else:
-            # First Layer is a std dense layer
-            hidden = layers.Dense(self.model_width, activation="elu", kernel_initializer=input_initializer,
-                                  kernel_regularizer=None, use_bias=True,
-                                  bias_initializer=input_initializer,
-                                  bias_regularizer=None, name="layer_-1_input")(x)
+
+        v = tf.concat([x,-x],axis=1)
+        # First Layer is a std dense layer
+        hidden = layers.Dense(self.model_width, activation="elu", kernel_initializer=input_initializer,
+                              kernel_regularizer=None, use_bias=True,
+                              bias_initializer=input_initializer,
+                              bias_regularizer=None, name="layer_-1_input")(v)
         # other layers are convexLayers
         for idx in range(0, self.model_depth):
-            hidden = convex_layer(hidden, x, layer_idx=idx, layer_dim=self.model_width)
-        pre_output = convex_output_layer(hidden, x, layer_idx=self.model_depth + 2)  # outputlayer
+            hidden = convex_layer(hidden, v, layer_idx=idx, layer_dim=self.model_width)
+        pre_output = convex_output_layer(hidden, v, layer_idx=self.model_depth + 2)  # outputlayer
 
         # Create the core model
         core_model = keras.Model(inputs=[input_], outputs=[
