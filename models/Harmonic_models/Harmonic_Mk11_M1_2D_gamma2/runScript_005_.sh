81f8d4af7fcb614d43b5d7ef11ef4fd935f07ccc
ScSteffen <steffen.schotthoefer@outlook.de>
2023-09-15 14:54:02 +0200

    added models for rotation tests

---
+++ models/Harmonic_models/Harmonic_Mk11_M1_2D_gamma2/runScript_005_.sh
@@ -0,0 +1,19 @@
+python callNeuralClosure.py \
+--sampling=2 \
+--batch=64 \
+--curriculum=1 \
+--degree=2 \
+--epoch=30 \
+--folder=Harmonic_Mk11_M1_2D_gamma2 \
+--loadModel=1 \
+--model=11 \
+--normalized=1 \
+--scaledOutput=0 \
+--decorrInput=1 \
+--objective=2 \
+--processingmode=1 \
+--spatialDimension=2 \
+--training=1 \
+--verbosity=2 \
+--networkwidth=350 \
+--networkdepth=3
---
+++ models/Harmonic_models/Harmonic_Mk11_M1_2D_gamma3/runScript_003_.sh
@@ -0,0 +1,19 @@
+python callNeuralClosure.py \
+--sampling=2 \
+--batch=32 \
+--curriculum=1 \
+--degree=2 \
+--epoch=20 \
+--folder=Harmonic_Mk11_M1_2D_gamma3 \
+--loadModel=1 \
+--model=11 \
+--normalized=1 \
+--scaledOutput=0 \
+--decorrInput=1 \
+--objective=0 \
+--processingmode=1 \
+--spatialDimension=2 \
+--training=1 \
+--verbosity=2 \
+--networkwidth=350 \
+--networkdepth=3
